d769f8257c4b463a3d34c022ba5580d885cf9c8c
chiaxiong <cyx.cyxiong@gmail.com>
2021-10-22 12:13:29 -0500

    moving components around

--- client/components/ProductDetails/ProductListing.jsx
+++ client/components/ProductDetails/ProductListing.jsx
@@ -1,22 +1,9 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchProdcutInfo } from '../../state/productSlice';
 
-const ProductListings = () => {
-  // {products} comes from our store.js
-  const { products } = useSelector((state) => state.allProducts);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(fetchProdcutInfo());
-  }, []);
-
+const ProductListings = (props) => {
   return (
     <div>
-      Product listing
-      {products.map((product) => (
-        <h3>{product.title}</h3>
-      ))}
+      <h1>{props.title}</h1>
+      <img src={props.image} alt={props.title} />
     </div>
   );
 };
--- client/components/ProductDetails/index.jsx
+++ client/components/ProductDetails/index.jsx
@@ -1,11 +1,24 @@
-import React from "react";
-import ProductListings from "./ProductListing";
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchProdcutInfo } from '../../state/productSlice';
+import ProductListings from './ProductListing';
 
 const ProductPage = () => {
+  // {products} comes from our store.js
+  const { products } = useSelector((state) => state.allProducts);
+  const dispatch = useDispatch();
+
+  console.log(products);
+
+  useEffect(() => {
+    dispatch(fetchProdcutInfo());
+  }, [dispatch]);
   return (
     <div>
       <h1>Product page</h1>
-      <ProductListings />
+      {products.map((product) => (
+        <ProductListings {...product} />
+      ))}
     </div>
   );
 };
