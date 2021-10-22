5f1107d318d665d1ae1b22ba4d01b47e772f4062
chiaxiong <cyx.cyxiong@gmail.com>
2021-10-22 13:45:08 -0500

    add comments to selector folder

--- client/state/selectors/product.js
+++ client/state/selectors/product.js
@@ -1,5 +1,8 @@
 import { createSelector } from '@reduxjs/toolkit';
 
+//we use a createSelector in the case we use it in multiple
+//places and need to make changes to it
+
 //1. create your variable
 //2. pass in your state, in this case we destructed
 //3. grab the info from you Redux tree
